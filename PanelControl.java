614f73243f330eca870d93df34d0fc5cfd732584
Ta Huu Tu <tahuutu03@gmail.com>
2024-05-07 21:57:13 +0700

    update

--- PanelControl.java
+++ PanelControl.java
@@ -1,3 +1,4 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -10,6 +11,7 @@ import javax.swing.SwingConstants;
 
 public class PanelControl extends JPanel {
 	public static JLabel jLabel;
+
 	public JLabel jLabel2;
 	public static Font font = new Font("TimeNewRoman", Font.BOLD, 15);
 //	public static Border border = BorderFactory.createLineBorder(Color.BLACK, 5);
@@ -20,11 +22,21 @@ public class PanelControl extends JPanel {
 		jLabel.setFont(font);
 		jLabel.setForeground(Color.blue);
 		setPreferredSize(new Dimension(200, 600));
-		setLayout(new GridLayout(6, 1));
+		setLayout(new BorderLayout());
 		setBackground(Color.LIGHT_GRAY);
-		add(jLabel2);
-		add(jLabel);
-//		setBorder(border);
+		add(jLabel2, BorderLayout.NORTH);
+		add(jLabel, BorderLayout.CENTER);
+		JPanel jPanel = new JPanel();
+
+		JButton restart = new Restart();
+
+		JButton exit = new Exit();
+
+		jPanel.setLayout(new GridLayout(2, 1));
+		jPanel.add(restart);
+		jPanel.add(exit);
+		jPanel.setBackground(Color.LIGHT_GRAY);
+		add(jPanel, BorderLayout.SOUTH);
 
 	}
 
@@ -35,19 +47,19 @@ public class PanelControl extends JPanel {
 			jLabel.setText("Đến lượt đội xanh dương!");
 			jLabel.setFont(font);
 			jLabel.setForeground(Color.blue);
-//			border = BorderFactory.createLineBorder(Color.blue, 5);
+
 		}
 		if (GameManager.currentPlayer == 1) {
 			jLabel.setText("Đến lượt đội đỏ!");
 			jLabel.setFont(font);
 			jLabel.setForeground(Color.red);
-//			border = BorderFactory.createLineBorder(Color.red, 5);
+
 		}
 		if (GameManager.currentPlayer == 2) {
 			jLabel.setText("Đến lượt đội xanh lá!");
 			jLabel.setFont(font);
 			jLabel.setForeground(Color.green);
-//			border = BorderFactory.createLineBorder(Color.green, 5);
+
 		}
 		if (GameManager.currentPlayer == 3) {
 			jLabel.setText("Đến lượt đội vàng!");
@@ -57,7 +69,4 @@ public class PanelControl extends JPanel {
 		}
 
 	}
-
-	public static JButton button1;
-	public static JButton button2;
 }
